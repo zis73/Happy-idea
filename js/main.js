c164f3522d75df7b174210d201ffafe52823b657
Eldar Akavov <akavoveldar3005@gmail.com>
2022-01-14 20:56:23 +0300

    chanche swiper animation at 375 and 429

--- js/main.js
+++ js/main.js
@@ -7,6 +7,41 @@ const swiper = new Swiper('.services-swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  speed:0,
+  autoplay:{
+    delay:0,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween:0,
+      speed:2000,
+      autoplay:{
+        delay:2500,
+        disableOnInteraction: false
+      },
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween:0,
+      speed:0,
+      autoplay:{
+        delay:0,
+        disableOnInteraction: false
+      },
+    },
+    769: {
+      slidesPerView: 1,
+      spaceBetween:0,
+      speed:0,
+      autoplay:{
+        delay:0,
+        disableOnInteraction: false
+      },
+    },
+  },
   // autoHeight: true,
   // slidesPerView: 1,
   // centeredSlides: true,
@@ -32,7 +67,41 @@ const swiper2 = new Swiper('.prices-swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
+  speed:0,
+  autoplay:{
+    delay:0,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween:0,
+      speed:2000,
+      autoplay:{
+        delay:2500,
+        disableOnInteraction: false
+      },
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween:0,
+      speed:0,
+      autoplay:{
+        delay:0,
+        disableOnInteraction: false
+      },
+    },
+    769: {
+      slidesPerView: 1,
+      spaceBetween:0,
+      speed:0,
+      autoplay:{
+        delay:0,
+        disableOnInteraction: false
+      },
+    },
+  },
   // autoHeight: true,
   // slidesPerView: 1,
   // centeredSlides: true,
@@ -63,6 +132,36 @@ const swiper3 = new Swiper('.clients-swiper', {
     delay:0,
     disableOnInteraction: false
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween:60,
+      speed:1200,
+      autoplay:{
+        delay:1800,
+        disableOnInteraction: false
+      },
+    },
+    480: {
+      slidesPerView: 4,
+      spaceBetween:60,
+      speed:1200,
+      autoplay:{
+        delay:1800,
+        disableOnInteraction: false
+      },
+    },
+    769: {
+      slidesPerView: 5,
+      spaceBetween:60,
+      speed:1200,
+      autoplay:{
+        delay:1800,
+        disableOnInteraction: false
+      },
+    },
+  },
 });
 const swiper4 = new Swiper('.hello-block-swiper', {
   //Бесконечная прокрутка
@@ -81,5 +180,6 @@ const swiper4 = new Swiper('.hello-block-swiper', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+  
 });
 document.querySelector('video').playbackRate = 0.65;
